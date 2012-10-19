b8eb5916a35ad20877486d952e4dc61fdff372f8
Kashinskaya Yana <kashin.yana@gmail.com>
2012-10-20 01:44:16 +0400

    fix -a in counter

---
+++ src/ru/fizteh/fivt/students/kashinYana/parallelSort/ParallelSort.java
@@ -0,0 +1,11 @@
+package ru.fizteh.fivt.students.kashinYana.parallelSort;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yana
+ * Date: 14.10.12
+ * Time: 22:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParallelSort {
+}
